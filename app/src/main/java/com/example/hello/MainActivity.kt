bcd1181f798f7cea5673b9ad5881cdae9f293c81
101 <tmddnjs001133@dsm.hs.kr>
2023-12-01 15:30:04 +0900

    현재 시간

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hello
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
@@ -33,20 +35,41 @@ class MainActivity : AppCompatActivity() {
             tv.text = "안드로이드"
         }
 
-        val button3 = findViewById<Button>(R.id.buttonToGetDate)
+        val button3 = findViewById<Button>(R.id.buttonToGetTime)
 
         button3.setOnClickListener {
+            getTime()
+        }
+
+        val button4 = findViewById<Button>(R.id.buttonToGetDate)
+
+        button4.setOnClickListener {
             getDate()
         }
 
     }
 
+    private fun getTime() {
+        val handler = Handler(Looper.getMainLooper())
+        handler.post(object : Runnable {
+            override fun run() {
+                val currentTime = Calendar.getInstance().time
+                val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+                val timeString = timeFormat.format(currentTime)
+                tv.text = "현재 시간 : $timeString"
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
+
+
     @SuppressLint("SetTextI18n")
     private fun getDate() {
-        val currentTime = Calendar.getInstance().time
-        val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-        val timeString = timeFormat.format(currentTime)
-        tv.text = "현재 시간 : $timeString"
+        val currentDate = Calendar.getInstance().time
+        val timeFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val timeString = timeFormat.format(currentDate)
+        tv.text = "오늘 날짜 : $timeString"
     }
+
 
 }
