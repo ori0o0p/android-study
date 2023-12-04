075d01a923a4692e8596ce3d4994bffa13cf0c1f
101 <tmddnjs001133@dsm.hs.kr>
2023-12-04 10:41:04 +0900

    수정

--- app/src/main/java/com/example/hello/MainActivity.kt
+++ app/src/main/java/com/example/hello/MainActivity.kt
@@ -15,6 +15,8 @@ import java.util.Locale
 class MainActivity : AppCompatActivity() {
 
     private lateinit var tv : TextView
+    private lateinit var handler: Handler
+    private lateinit var runnable: Runnable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,11 +30,13 @@ class MainActivity : AppCompatActivity() {
         val button = findViewById<Button>(R.id.buttonToEn)
         val button2 = findViewById<Button>(R.id.buttonToKor)
         button.setOnClickListener {
+            handler.removeCallbacks(runnable) // 시간 갱신 작업 중단
             buttonClickMessage("영어 선택")
             tv.text = "Android"
         }
 
         button2.setOnClickListener {
+            handler.removeCallbacks(runnable) // 시간 갱신 작업 중단
             buttonClickMessage("한국어 선택")
             tv.text = "안드로이드"
         }
@@ -47,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         val button4 = findViewById<Button>(R.id.buttonToGetDate)
 
         button4.setOnClickListener { // 날짜
+            handler.removeCallbacks(runnable) // 시간 갱신 작업 중단
             buttonClickMessage("오늘 날짜 선택")
             getDate()
         }
@@ -54,17 +59,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getTime() {
-        val handler = Handler(Looper.getMainLooper())
-        handler.post(object : Runnable {
-            override fun run() {
-                val currentTime = Calendar.getInstance().time
-                val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
-                val timeString = timeFormat.format(currentTime)
-                tv.text = "현재 시간 : $timeString"
-                handler.postDelayed(this, 1000)
-                // handler.removeCallbacks(this)
-            }
-        })
+        handler = Handler(Looper.getMainLooper())
+        runnable = Runnable {
+            val currentTime = Calendar.getInstance().time
+            val timeFormat = SimpleDateFormat("HH:mm:ss", Locale.getDefault())
+            val timeString = timeFormat.format(currentTime)
+            tv.text = "현재 시간 : $timeString"
+            handler.postDelayed(runnable, 1000)
+        }
+        handler.post(runnable)
     }
 
     private fun buttonClickMessage(message : String) {
